5b4190e6228c6415d72217dd69f9fc805cac45ab
netoe1 <elynetobr@outlook.com>
2025-07-23 14:37:34 -0300

    add new gates! update license! lets roll

--- gates.c
+++ gates.c
@@ -1,3 +1,18 @@
+/* Copyright 2025 Ely Torres Neto
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include "gates.h"
@@ -21,8 +36,20 @@ Bit gateBitToBit(Gates_Types GATE_SELECT, Bit bits[], size_t n)
     case OR:
         funct = bit_or;
         break;
+    case XNOR:
+        funct = bit_xnor;
+        break;
+    case NOR:
+        funct = bit_nor;
+        break;
+    case NAND:
+        funct = bit_nand;
+        break;
+    case XOR:
+        funct = bit_xor;
+        break;
     default:
-        puts("Error: Invalid gate type!");
+        puts("gates.c:gateBitToBit(): Invalid gate type!");
         exit(-1);
     }
 
